8dcffbc35c5b6e94e0931b69ab1dded068b6cba7
Siyuan-Harry <siyuanfang@foxmail.com>
2023-10-23 13:41:36 +0800

    小修改st.input和session代码结构的问题

--- try_my_app.py
+++ try_my_app.py
@@ -203,59 +203,61 @@ def app():
     
     
     col1, col2 = st.columns(2)
-    
+
+    with col1:
+        if btn:
+            temp_file_paths = []
+            with st.spinner("Processing file..."):
+                for added_file in added_files:
+                    with tempfile.NamedTemporaryFile(delete=False, suffix=".md") as tmp:
+                        tmp.write(added_file.getvalue())
+                        tmp_path = tmp.name
+                        temp_file_paths.append(tmp_path)
+            courseOutline, course_content_list = main(temp_file_paths, num_lessons)
+
+            st.text_area("Course Outline", value=courseOutline)
+            st.text_area("Course Content", value=course_content_list)
+
+    # Set a default model
+        if "openai_model" not in st.session_state:
+            st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        #这里的session.state就是保存了这个对话会话的一些基本信息和设置
+        
     if prompt := st.chat_input("What is up?"):
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
         # Display user message in chat message container
 
-        with col1:
-            if btn:
-                temp_file_paths = []
-                with st.spinner("Processing file..."):
-                    for added_file in added_files:
-                        with tempfile.NamedTemporaryFile(delete=False, suffix=".md") as tmp:
-                            tmp.write(added_file.getvalue())
-                            tmp_path = tmp.name
-                            temp_file_paths.append(tmp_path)
-                courseOutline, course_content_list = main(temp_file_paths, num_lessons)
+    with col2:
+        st.title("chatbot assistant")
 
-                st.text_area("Course Outline", value=courseOutline)
-                st.text_area("Course Content", value=course_content_list)
+        
 
-        with col2:
-            st.title("chatbot assistant")
-
-            # Set a default model
-            if "openai_model" not in st.session_state:
-                st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-            # Initialize chat history
-            if "messages" not in st.session_state:
-                st.session_state.messages = []
-
-            # Display chat messages from history on app rerun
-            for message in st.session_state.messages:
-                with st.chat_message(message["role"]):
-                    st.markdown(message["content"])
-
-            #这里的session.state就是保存了这个对话会话的一些基本信息和设置
-
-            with st.chat_message("user"):
-                st.markdown(prompt)
-            # Display assistant response in chat message container
-            with st.chat_message("assistant"):
-                message_placeholder = st.empty()
-                full_response = ""
-                for response in openai.ChatCompletion.create(
-                    model=st.session_state["openai_model"],
-                    messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-                    stream=True,
-                ):
-                    full_response += response.choices[0].delta.get("content", "")
-                    message_placeholder.markdown(full_response + "▌")
-                message_placeholder.markdown(full_response)
-            st.session_state.messages.append({"role": "assistant", "content": full_response})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            for response in openai.ChatCompletion.create(
+                model=st.session_state["openai_model"],
+                messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+                stream=True,
+            ):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
     
 if __name__ == "__main__":
