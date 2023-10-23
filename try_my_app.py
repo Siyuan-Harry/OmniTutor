2b9a88e6c1f89ead39d7d9829df7dc19d7788854
Siyuan-Harry <siyuanfang@foxmail.com>
2023-10-23 14:36:13 +0800

    try

--- try_my_app.py
+++ try_my_app.py
@@ -205,8 +205,8 @@ def app():
     col1, col2 = st.columns([0.6,0.4], gap='large')
 
     with col1:
-        st.text_area("Course Outline")
-        st.text_area("Course Content")
+        st.text_area("Course Outline", "This area is filled with course outline", disabled=True)
+        st.text_area("Course Content", 'This area is filled with course content', disabled=False)
         if btn:
             temp_file_paths = []
             with st.spinner("Processing file..."):
