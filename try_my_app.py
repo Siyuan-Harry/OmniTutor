bad19cc8371b3c3af17ac1ee3939e13fdf4d993b
Siyuan-Harry <siyuanfang@foxmail.com>
2023-10-23 14:08:20 +0800

    迭代ui

--- try_my_app.py
+++ try_my_app.py
@@ -220,26 +220,24 @@ def app():
             st.text_area("Course Outline", value=courseOutline)
             st.text_area("Course Content", value=course_content_list)
 
-    # Set a default model
-    if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-    #这里的session.state就是保存了这个对话会话的一些基本信息和设置
-
     prompt = st.chat_input("What is up?")
         # Add user message to chat history
 
     with col2:
         st.caption(''':blue[AI Assistant]: Ask me any questions related to this coourse and I'll give you direct answer. :sunglasses:''')
+            # Set a default model
+        if "openai_model" not in st.session_state:
+            st.session_state["openai_model"] = "gpt-3.5-turbo"
 
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+        #这里的session.state就是保存了这个对话会话的一些基本信息和设置
         if prompt:
             st.session_state.messages.append({"role": "user", "content": prompt})
             with st.chat_message("user"):
